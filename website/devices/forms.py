965e7dd683a16b5167ac84f620efa8eca6e770c1
ntwklab <113145255+ntwklab@users.noreply.github.com>
2022-09-30 18:04:00 +0100

    Delete website directory

--- website/devices/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, IntegerField, SubmitField, RadioField, EmailField, StringField
-from wtforms.validators import DataRequired
-
-
-class DeviceForm(FlaskForm):
-        ip = TextAreaField('IP Address of Device', validators=[DataRequired()])
-        submit = SubmitField('Submit')
